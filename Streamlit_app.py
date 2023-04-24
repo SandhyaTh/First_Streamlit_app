94122745abe235cb0b219521ea4da84c6ed403fe
SandhyaTh <127309552+SandhyaTh@users.noreply.github.com>
2023-04-24 16:52:54 +0100

    Update Streamlit_app.py
    added Breakfast menu items

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit
 streamlit.title('My parents new healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Orange Juice')
+streamlit.text('Boiled Eggs')
+streamlit.text('Oat Cereal')
